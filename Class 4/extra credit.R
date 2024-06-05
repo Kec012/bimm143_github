a57b6c6d282b329b97456889042aab6dc5020ab3
Kevin Chen <kec012@ucsd.edu>
2024-06-04 18:50:59 -0700

    done

--- Class 4/extra credit.R
+++
@@ -1,7 +0,0 @@
-# lab 4 extra credit
-source("http://thegrantlab.org/misc/cdc.R")
-tail(cdc$height,20)
-plot(cdc$height,cdc$weight)
-weight_kg <- cdc$weight * 0.454
-bmi <- (weight_kg)/(height_m^2)
-table(cdc[bmi >= 30,9])
